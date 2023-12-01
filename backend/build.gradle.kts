67806c28f3e54a3fdd85db6a99ea4b4d062ebcba
Compilin <git@compilin.dev>
2023-12-01 18:29:57 +0100

    Added Ktor gradle plugin for fat jar bundling

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("ludoka.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
